1577243bf6f1e71d1cfad418ad1c6f4f09af3074
Justine <justine.e2405045@etud.univ-ubs.fr>
2026-04-16 19:36:36 +0200

    q28 + modifs q27 + rapport 27/28

--- src-27/server.py
+++ src-27/server.py
@@ -1,20 +1,22 @@
 import socket
 import json
-import mysql.connector
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('q27.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS employees
+                 (id INTEGER PRIMARY KEY, name TEXT, salary_ope INTEGER, salary_paillier TEXT)''')
+    conn.commit()
+    conn.close()
 
 def run_server():
-    db = mysql.connector.connect(
-        host="localhost",
-        user="tp_admin",
-        password="password_tp",
-        database="TP_SecuBDD"
-    )
-    cursor = db.cursor()
+    init_db()
 
     srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     srv.bind(('0.0.0.0', 5000))
     srv.listen(1)
-
+    
     while True:
         conn, addr = srv.accept()
         data = conn.recv(8192).decode()
@@ -23,10 +25,13 @@ def run_server():
         req = json.loads(data)
         action = req.get('action')
 
+        db = sqlite3.connect('q27.db')
+        cursor = db.cursor()
+
         if action == 'add':
             cursor.execute(
-                "INSERT INTO employees (id, name, salary_ope, salary_paillier) VALUES (%s, %s, %s, %s)",
-                (req['id'], req['name'], req['s_ope'], req['s_paillier'])
+                "INSERT INTO employees (id, name, salary_ope, salary_paillier) VALUES (?, ?, ?, ?)",
+                (req['id'], req['name'], req['s_ope'], str(req['s_paillier']))
             )
             db.commit()
             conn.send(b"OK")
@@ -37,7 +42,7 @@ def run_server():
 
         elif action == 'compare':
             cursor.execute(
-                "SELECT name FROM employees WHERE id IN (%s, %s) ORDER BY salary_ope DESC", 
+                "SELECT name FROM employees WHERE id IN (?, ?) ORDER BY salary_ope DESC", 
                 (req['id1'], req['id2'])
             )
             res = cursor.fetchall()
@@ -46,12 +51,17 @@ def run_server():
         elif action == 'sum':
             cursor.execute("SELECT salary_paillier FROM employees")
             encrypted_salaries = [int(r[0]) for r in cursor.fetchall()]
-            n_sq = int(req['n_sq'])
-            result_encrypted = 1
-            for s in encrypted_salaries:
-                result_encrypted = (result_encrypted * s) % n_sq
-            conn.send(str(result_encrypted).encode())
+            
+            if not encrypted_salaries:
+                conn.send(b"1")
+            else:
+                n_sq = int(req['n_sq'])
+                result_encrypted = 1
+                for s in encrypted_salaries:
+                    result_encrypted = (result_encrypted * s) % n_sq
+                conn.send(str(result_encrypted).encode())
 
+        db.close()
         conn.close()
 
 if __name__ == "__main__":
